00d1a6d5bd629b1ad691a73ab676f321841efc6d
mike <Mike Sanderson>
2019-06-20 20:10:26 -0400

    Basic template starting point

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
+from flask.templating import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/<greeting>')
+def hello_world(greeting="Hello"):
+    return render_template('hello.html', hello=greeting)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
