76ba233cefb18f1e2255f3eeb69beb508d73af63
mike <Mike Sanderson>
2019-06-23 14:32:39 -0400

    Added file uploading v0.1

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect
 from flask.templating import render_template
 from db import init as db_init
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = r'/home/mike/PycharmProjects/word_server/temp_docs'
+ALLOWED_EXTENSIONS = {'.txt', }
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'blahblah'
 
 config_obj = {
     'db': 'sqlite',
@@ -10,6 +17,21 @@ config_obj = {
 }
 
 db = db_init(config_obj)
+
+
+def store_document(file):
+    if file.filename == '':
+        return False, 'Please supply a file name'
+    if not ('.' in file.filename and os.path.splitext(file.filename)[1] in ALLOWED_EXTENSIONS):
+        return False, 'Only .txt files are currently supported'
+
+    filename = secure_filename(file.filename)
+    uploaded_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(uploaded_file)
+
+    db.add_document(uploaded_file, config_obj)
+
+    return True, "File Successfully Uploaded"
 
 
 @app.route('/')
@@ -23,9 +45,30 @@ def doc_index():
     return render_template('document_index.html', doc_index=doc_index)
 
 
-@app.route('/document_upload')
+@app.route('/document_upload', methods=('GET', 'POST'))
 def doc_upload():
-    return app.send_static_file('upload_form.html')
+    if request.method == 'GET':
+        return app.send_static_file('upload_form.html')
+
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            flash('Please provide a file')
+            return
+
+        success, msg = store_document(request.files['file'])
+
+        flash(msg)
+
+        if success:
+            return redirect('/{}'.format(request.files['file'].filename))
+        else:
+            return redirect('/document_upload')
+
+
+@app.route('/<document_name>')
+def document_page(document_name):
+    return redirect('/document_index')
 
 
 if __name__ == '__main__':
--- db_interfaces/sqlite.py
+++ db_interfaces/sqlite.py
@@ -1,16 +1,47 @@
 import sqlite3
+from hashlib import sha256
+import os
+import time
 
 
-def _init_sqlite_db(file):
-    conn = sqlite3.connect(file)
-    curs = conn.cursor()
+def _get_conn(config):
+    return sqlite3.connect(config['db_host'])
 
-    curs.execute('Create table if not exists raw_documents (doc_name text, doc_hash text, doc_body text);')
+
+def add_document(file_path, config):
+    m = sha256()
+    with open(file_path, 'rb') as fi:
+        doc_text = fi.read()
+        m.update(doc_text)
+
+    doc_hash = m.digest()
+    doc_name = os.path.split(file_path)[1]
+
+    conn = _get_conn(config)
+
+    try:
+        conn.execute('''insert into raw_documents(doc_hash, doc_name, doc_body, update_epoch_time)
+                    values (?, ?, ?, ?); ''', ((doc_hash, doc_name, doc_text, int(time.time()))))
+    except sqlite3.IntegrityError as e:
+        if 'UNIQUE' in e.args[0]:
+            pass
+        else:
+            print(e)
+            raise e
 
     conn.commit()
 
 
 def init(config):
-    file = config['db_host']
-    _init_sqlite_db(file)
+    conn = _get_conn(config)
+    curs = conn.cursor()
+
+    curs.execute('''Create table if not exists raw_documents 
+    (doc_hash text primary key, doc_name text, doc_body text, update_epoch_time int);''')
+    curs.execute('''Create table if not exists words (doc_hash text, word text, count int,
+                 Foreign Key (doc_hash) references raw_doucments(doc_hash),
+                 Constraint words_pk unique (doc_hash, word));''')
+    #curs.execute('Create unique index words_pk on words(doc_hash, word);')
+
+    conn.commit()
 
